cf14a3d024e7a668631e85eb40670da26407dc4c
Mirko <18010385@student.hhs.nl>
2021-09-28 12:18:57 +0200

    Nu kunnen we data ophalen en laten zien op streamlit

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import numpy as np
 import requests
 import plotly.figure_factory as ff
 from st_aggrid import AgGrid
+import io
 
 
 def download_data():
@@ -32,12 +33,9 @@ def download_data():
     download_url = get_file_response.json().get("temporaryDownloadUrl")
     dataset_file_response = requests.get(download_url)
 
-    return dataset_file_response.text
+    da = io.StringIO(dataset_file_response.text)
+    data = pd.read_csv(da, skip_blank_lines=True, skiprows=13, delim_whitespace=True)
 
-    # with open('Output.txt', 'w') as f:
-    #     f.write(dataset_file_response.text)
-
-    data = pd.read_csv('Output.txt', skip_blank_lines=True, skiprows=13, delim_whitespace=True)
     data.set_axis(
         ['Datum', "Valkenburg ZH", "De Kooy NH", "Schiphol NH", "De Bilt UT", "Leeuwarden FR", "Deelen GE", "Eelde GR",
          "Twenthe DR", "Vlissingen ZE", "Rotterdam ZH", "Gilze-Rijen NB", "Eindhoven NB", "Volkel NB", "Maastricht LI"],
@@ -46,40 +44,18 @@ def download_data():
     return data
 
 
+def clean_data(dataframe):
+
+
+    return dataframe
+
+
+
 st.title('Hallo')
 
 st.markdown("tweede test")
 
+data = download_data()
+data_clean = clean_data(data)
 
-
-
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-# st.write(download_data())
-
-# st.map(map_data)
-
-
-
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-       hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
-
-
+st.write(data['Valkenburg ZH'])
