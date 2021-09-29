3f996f4e482185906538ffab888ca06a1f749dbc
Mirko <18010385@student.hhs.nl>
2021-09-29 13:06:47 +0200

    Eerste opzet van de tekst er in gezet.

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ def download_data():
     get_file_response = requests.get(endpoint, headers={"Authorization": api_key})
     # print(get_file_response.json())
 
-    file_nr = 2  # Maakt het uit welke we kiezen???
+    file_nr = 0  # Maakt het uit welke we kiezen???
     filename = get_file_response.json()['files'][file_nr]['filename']
 
     endpoint = f"{api_url}/{api_version}/datasets/{dataset_name}/versions/{dataset_version}/files/{filename}/url"
@@ -36,12 +36,9 @@ def download_data():
     dataset_file_response = requests.get(download_url)
 
     da = io.StringIO(dataset_file_response.text)
-    data = pd.read_csv(da, skip_blank_lines=True, skiprows=13, delim_whitespace=True)
+    data = pd.read_csv(da, skip_blank_lines=True, skiprows=7, delim_whitespace=True)
 
-    data.set_axis(
-        ['Datum', "Valkenburg ZH", "De Kooy NH", "Schiphol NH", "De Bilt UT", "Leeuwarden FR", "Deelen GE", "Eelde GR",
-         "Twenthe DR", "Vlissingen ZE", "Rotterdam ZH", "Gilze-Rijen NB", "Eindhoven NB", "Volkel NB", "Maastricht LI"],
-        axis=1, inplace=True)
+
 
     return data
 
@@ -301,32 +298,103 @@ def line_chart(data2):
 # Eerst maar is de data downloaden van de KNMI website
 data = download_data()
 
+
+
+
+st.title('Case 2: Blogpost')
+st.write(' 	- Marcel Zwagerman	500889946\n'
+         '  - Sjoerd Fijne		500828895\n'
+         '  - Mirko Bosch		500888784\n'
+         '  - Martijn Draper	500888847')
+
+st.title('Inhoudsopgave')
+st.write('  - Samenvatting\n'
+         '  - API ophalen van KNMI\n'
+         '  - Data bewerken\n'
+         '  - Visualisatie')
+
+
+st.title('Samenvatting')
+st.write('Wij hebben een API opgehaald van de KNMI website. Deze data hebben we eerst opgeschoond en gegevens toegevoegd zoals het gemiddelde en standaard deviatie. We hebben vervolgens een globale weergave gemaakt en zijn opzoek gegaan naar een gemiddelde temperatuur stijging, ......')
+
+
+st.title('API ophalen van KNMI')
+st.write('Een API ophalen van het KNMI. KNMI staat voor Koninklijk Nederlands Meteorologisch Instituut.Deze website is van de overheid en delen openbare API\'s waarvan wij er een hebben gebruikt.Deze website legt volledig uit hoe je een API kan inladen in je Jupiter Notebook. We hebben het stappenplan volledig gevolgd en uiteindelijk ingeladen in ons notebook. Hieronder zie je een schematische weergave van de data tabel die we hebben ingeladen.')
+
+st.write(data.head(7))
+
+st.write("Deze data is nu ingeladen maar de eerste 4 regels zijn afwijkend van de rest. Als we kijken naar wat dit is zien we onderanderen de coördinaten. De kolom zijn nu de stations nummers.")
+
+link = '[KNMI](https://developer.dataplatform.knmi.nl/example-scripts)'
+st.markdown(link, unsafe_allow_html=True)
+
+st.title('Data bewerken')
+st.write('Voordat we echt kunnen gaan spelen met onze data moeten we de data opschonen. Dit hebben we gedaan door eerst de kolommen een naam te geven. Dit konden we afleiden van het KNMI website. Hier is bijvoorbeeld nummer 240 de locatie op Schiphol. Hieronder zie je een afbeelding waar zo makelijk te herleiden is welk nummer bij welk station hoort.')
+
+# Afbeelding toevoegen: KNMI_meetpunten.png
+
+st.write('Na het toevoegen van de kolommen kwamen we erachter dat we nog een aantal rijen moesten laten vallen, want die behoorde niet tot de dataset. We hebben ook een controlle gedaan of er missende gegevens zijn, maar gelukkig waren er geen missende gegevens. \n \nWe hebben de dataset gekoppieerd waarbij een dataset de datum als een index is en een dataset waarbij de datum een kolom is. Dit hebben we gedaan voor enkele visualisaties die je later in deze blog gaat zien. Na deze aanpassingen hebben we een gemiddelde en een standaard deviatie toegevoegd aan de datasets')
+
+data.set_axis(
+        ['Datum', "Valkenburg ZH", "De Kooy NH", "Schiphol NH", "De Bilt UT", "Leeuwarden FR", "Deelen GE", "Eelde GR",
+         "Twenthe DR", "Vlissingen ZE", "Rotterdam ZH", "Gilze-Rijen NB", "Eindhoven NB", "Volkel NB", "Maastricht LI"],
+        axis=1, inplace=True)
+
 #de data even netjes zetten en een gemiddelde in gooien.
 data2 = data.drop(index=[0,1,2,3])
 data2['Gemiddelde'] = round(data2.iloc[:,1:14].mean(axis=1),1)
 # data2.head() # De kolom 'Datum' is op deze manier niet handig dus die verwijderen
 # Vervolgens een nieuwe kolom voor de datum invoegen die wel correct is (time series)
 
+data3 = data2.drop(columns='Datum')
+data3['Datum'] = pd.date_range("1981-01-01", periods=len(data3), freq="D")
+data_clean = data3.set_index('Datum')
+# data_clean.head() # Nu is de data clean
+
+# We voegen een kolom toe met de gemiddelde temperatuur van alle weerstations
+# En ook een kolom met de standaarddeviatie
+data_clean['Gemiddelde'] = round(data_clean.mean(axis=1),1)
+data_clean['Standaarddeviatie'] = round(data_clean.std(axis=1),1)
 
 
+st.write(data2.head())
+st.write(data_clean.head())
 
 
-st.title('Hoe varrieert de data in Nederland in de periode van 1981 tot 2010')
+st.title('Visualisatie')
+st.header('Temperatuur in Nederland, 1981 - 2011')
 
-# st.code(download_data.py)
-
-
-
-
-
-st.title("Hoe is de temperatuur over de periode per meet punt?")
 fig = line_chart(data2)
 fig.update_layout(height=500, width=1000)
 
 st.plotly_chart(fig)
-fig
 
+st.write('Deze interactieve lijngrafiek gebruikt alle data van de dataset. Op de verticale as is de temperatuur te lezen en op de horizontale as de tijd. Elke meetpunt kan worden geselecteerd en de periode kan je zelf ook aangeven door de selecteren. Dit is een duidelijke globale weergave van de dataset waarmee we nu het volgende mee wilden onderzoeken:\n'
+         '- Is er sprake van opwarming van de aarde en hoeveel dan?\n'
+         '- Boxplot?\n'
+         '- Histogram?\n'
+         '- ahhhh')
 
+st.header('Gemiddelde temperatuur per maand, 1981 - 2011')
 
+st.title("Hier een grafiek")
 
-# st.write(data['Valkenburg ZH'])
+st.write('Deze spreidingsdiagram geeft een globale weergave aan van temperatuurstijging in Nederland. We moeten er meteen bijzeggen dat dit te weinig data hebben waardoor er toevalligheid kan zijn. En we hebben het gemiddelde genomen van elke maand, dus dat veranderd de trendlijn ook een beetje. (Daarvoor hebben we een tweede grafiek gemaakt waar wel alle data is gebruikt en waar de trendline nauwkeuriger is.) We hebben het gemiddelde van elke maand een apparte kleur gegeven zodat je duidelijk kan aflezen wat voornamelijkst de warmste maanden zijn en wat de koudste maanden.')
+
+st.header('Opwarming van de aarde')
+
+st.title("Hier een grafiek GRAFIEK 0.0447443 * Tijd (jaren) + -78.2203 0.000120045 * Tijd (dagen) + 10.4651")
+
+st.header('Extreme waardes')
+
+st.title("Hier een tabel")
+
+st.write('Hierboven zie je een tabel van de top 10 koudste en warmste dagen van de periode 1981 - 2011. Je moet nagaan dat dit de gemiddelde temperatuur is van de hele dag. Dus er is om de zoveel tijd weer nieuwe meting gedaan en daarvan is het gemiddelde gedaan. Dus dat wil zeggen dat 27,9 °C niet de maximale temperatuur is, maar het gemiddelde. "Juli 2006 warmste maand in zeker 300 jaar" zegt het KNMI.')
+
+st.title("Links")
+st.write("https://docs.streamlit.io/en/stable/getting_started.html#add-text-and-data"
+         "https://docs.streamlit.io/en/stable/api.html#magic-commands"
+         "https://www.clo.nl/indicatoren/nl022613-temperatuur-mondiaal-en-in-nederland"
+         "https://developer.dataplatform.knmi.nl/example-scripts"
+         "https://dataplatform.knmi.nl/dataset/knmi14-gemiddelde-temperatuur-3-2"
+         "https://discuss.streamlit.io/t/how-to-link-a-button-to-a-webpage/1661/7")
